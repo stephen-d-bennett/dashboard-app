b53b91ced441c7ee09b65e26cd3fb06c4105643a
stephen-d-bennett <stephen_bennett@me.com>
2026-07-27 14:24:36 -0500

    Refactor Supabase client initialization

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ alert("JS is running");
 // ---------------------------
 const supabaseUrl = "https://yykqbqdosdvigghaguvd.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5a3FicWRvc2R2aWdnaGFndXZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyMjg4MDIsImV4cCI6MjA5OTgwNDgwMn0.u-luZHqcc9aeHLGHIrWOPaK4vVXIHmwv3r3yZVvW7DU";
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 let topics = [];
