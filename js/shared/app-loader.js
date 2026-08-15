7cfe5f6443a41388a33d1c793c5f8e7b1bdb6092
stephen-d-bennett <309393716+stephen-d-bennett@users.noreply.github.com>
2026-08-15 17:20:28 -0500

    test

--- js/shared/app-loader.js
+++ js/shared/app-loader.js
@@ -1,3 +1,4 @@
+/*
 document.body.insertAdjacentHTML(
   "beforeend",
   "<div style='position:fixed;top:0;left:0;z-index:99999;background:red;color:white;padding:10px;font-size:20px;'>LOADER RAN</div>"
@@ -26,9 +27,13 @@ async function loadConfig() {
   css.href = `/css/${version}/app.css`;
   document.head.appendChild(css);
 
-  // ⭐ Architecturally correct dynamic import
+  // Architecturally correct dynamic import
   Debug.log("loader: importing ./" + version + "/app.js");
   await import(`./${version}/app.js`);
 
   Debug.log("loader: module loaded");
 })();
+*/
+
+console.log("LOADER RAN");
+document.body.style.border = "20px solid red";
