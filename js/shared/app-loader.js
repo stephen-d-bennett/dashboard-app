06721133d33fa206ef17425bd4cbcac64718a9a7
stephen-d-bennett <309393716+stephen-d-bennett@users.noreply.github.com>
2026-08-16 15:07:03 -0500

    Go back to v1

--- js/shared/app-loader.js
+++ js/shared/app-loader.js
@@ -1,34 +1,14 @@
 // FILE: /js/shared/app-loader.js
 
-import { ensureHUD, log } from "./js/shared/logger.js";
-
-async function loadConfig() {
-  const res = await fetch("/config/app-config.json", { cache: "no-store" });
-  return await res.json();
-}
-
-export async function start() {
-  ensureHUD();
-  log("loader: started");
-
-  const config = await loadConfig();
-  const version = config.version;
-
-  log("loader: version = " + version);
-
-  // Load CSS for the version
+async function start() {
+  // Load v1 CSS
   const css = document.createElement("link");
   css.rel = "stylesheet";
-  css.href = `/css/${version}/app.css`;
+  css.href = "/css/v1/app.css";
   document.head.appendChild(css);
 
-  log("loader: importing ../" + version + "/app.js");
-  const module = await import(`../${version}/app.js`);
-
-  log("loader: module loaded");
-
-  if (typeof module.init === "function") {
-    log("loader: calling init()");
-    await module.init();
-  }
+  // Load v1 JS
+  await import("/js/v1/app.js");
 }
+
+start();
