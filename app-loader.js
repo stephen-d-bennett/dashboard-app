088127ad268d3399432c0d0d19b41f0db723bac2
stephen-d-bennett <309393716+stephen-d-bennett@users.noreply.github.com>
2026-08-16 04:48:28 -0500

    hud

--- app-loader.js
+++ app-loader.js
@@ -1,10 +1,11 @@
-console.log("app-loader.js is running");
-
 import { log } from "./logger.js";
+import { start } from "./v2/app.js";
 
 log("app-loader.js loaded.");
 log("Calling init()...");
 
 export function init() {
   log("init() inside app-loader.js ran successfully.");
+  log("Calling v2/app.js start()...");
+  start();
 }
---
+++ v2/app.js
@@ -0,0 +1,7 @@
+import { log } from "../logger.js";
+
+log("v2/app.js loaded.");
+
+export function start() {
+  log("v2/app.js start() ran successfully.");
+}
