1d3e7c20f096691baa29f13fccefb490f3e02cf4
stephen-d-bennett <309393716+stephen-d-bennett@users.noreply.github.com>
2026-08-19 00:06:17 -0500

    v2 testing

--- js/v2/app.js
+++ js/v2/app.js
@@ -1,11 +1,20 @@
 // FILE: /js/v2/app.js
 export async function init() {
-    const app = document.getElementById("app");
+  
+  
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<div style="background:green; color:white; padding:10px;">
+       v2 init() RAN
+     </div>`
+  );
+   
+  const app = document.getElementById("app");
 
-    app.innerHTML = `
-        <div style="padding:20px; background:#222; color:#fff;">
-            <h1>v2 init() RAN</h1>
-            <p>If you see this, v2/app.js loaded and init() executed.</p>
-        </div>
+  app.innerHTML = `
+      <div style="padding:20px; background:#222; color:#fff;">
+          <h1>v2 init() RAN</h1>
+          <p>If you see this, v2/app.js loaded and init() executed.</p>
+      </div>
     `;
 }
