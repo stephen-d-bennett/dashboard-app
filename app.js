1b95b4c1b903f452aea21c71a663bc4cc8312301
stephen-d-bennett <309393716+stephen-d-bennett@users.noreply.github.com>
2026-08-19 00:42:18 -0500

    db

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ document.body.insertAdjacentHTML(
      app.js TOP-LEVEL RAN
    </div>`
 );
+document.body.insertAdjacentHTML(
+  "beforeend",
+  `<div style="background:green; color:white; padding:10px;">
+     END OF app.js
+   </div>`
+);
